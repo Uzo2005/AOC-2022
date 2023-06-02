e6da4b712bc6defc8a02ff00b323c5f94bf9bac9
Cletus <igwecletus455@gmail.com>
2023-06-02 02:31:31 +0100

    just solved day 9, lol, having fun

--- 9/a.nim
+++ 9/a.nim
@@ -1,106 +1,152 @@
-import strutils, sequtils, math, sugar, algorithm, os
+import strutils, sequtils, algorithm
 
-const data = readfile("data.txt").strip().splitLines()
+const data = readfile("data.txt").strip.splitLines.mapIt(it.strip.splitWhitespace)
+
 var 
-    realHeadPosition: (int, int)
-    realTailPosition: (int, int)
-    tailMovements: seq[(int, int)]
+    hPosX, hPosY: int
+    tPosX, tPosY: int
+    tailposlogger: seq[(int, int)]
 
-proc direction (data: string) : char =
-    result = data[0]
+proc updateTail =
+    if not(hposX != tposX and hposY != tposY) or not(abs(hPosX - tPosX) == 1 and hPosY == tPosY) or not(abs(hPosY - tPosY) == 1 and hPosX == tPosX):
+        #they are not touching and not too far from each other
+        
+        if hposX - tposX == 2 and hposY == tposY:
+            #head is 2 step to the right
+            echo "  -- head is 2 step to the right"
+            inc tposX
 
-proc distanceToMove(data: string) : int =
-    result = parseInt(data[2..^1])
+        elif tposX - hposX == 2 and hposY == tposY:
+            #head is 2 step to the left
+            echo "  -- head is 2 step to the left"
+            dec tposX
 
-proc isTwoHorizontalStepsAheadOf (headPosition, tailPosition: (int, int)) :bool =
-    let 
-        (headXDirection, headYDirection) = headPosition
-        (tailXDirection, tailYDirection) = tailPosition
+        elif hposY - tposY == 2 and hposX == tposX:
+            #head is 2 step above
+            echo "  -- head is 2 step above"
+            inc tposY
 
-    if (abs(headXDirection - tailXdirection) > 1 and headYDirection == tailYDirection) or (abs(headYDirection - tailYDirection) > 1 and headXDirection == tailXDirection):
-        result = true
-
-proc isDiagonallyAdjacentTo (headPosition, tailPosition: (int, int)) :bool =
-    let 
-        (headXDirection, headYDirection) = headPosition
-        (tailXDirection, tailYDirection) = tailPosition
-
-    if ((abs(headYDirection - tailYDirection) > 1) and (abs(headXDirection - tailXDirection) > 0)) or ((abs(headXDirection - tailXdirection) > 1) and (abs(headYDirection - tailYDirection) > 0)):
-        result = true
+        elif tposY - hposY == 2 and hposX == tposX:
+            #head is 2 step below
+            echo "  -- head is 2 step below"
+            dec tposY
+        
+        else:
+            echo "       -->no simple case is matched"
 
 
-for movement in data:
-    case movement.direction():
-    of 'R':
-        for eachStep in 1..movement.distanceToMove():
-            inc realHeadPosition[0]
-            if realHeadPosition.isDiagonallyAdjacentTo(realTailPosition):
-                # echo "diagonal Right"
-                if (realHeadPosition[1] - realTailPosition[1] ) >= 1:
-                    realTailPosition[1] += 1
-                else:
-                    realTailPosition[1] -= 1
-                inc realTailPosition[0]
-            elif realHeadPosition.isTwoHorizontalStepsAheadOf(realTailPosition):
-                # echo "Horizontal Right"
-                realTailPosition = (realHeadPosition[0]-1, realTailPosition[1])
-            else:
-                # echo "none of them"
-                discard
-            tailMovements.add(realTailPosition)
-        # echo "moved Right and distance moved is: ", movement.distanceToMove(), " and head position is ", realHeadPosition, " while tail position is: ", realTailPosition
-    of 'L':
-        for eachStep in 1..movement.distanceToMove():
-            realHeadPosition[0] -= 1
-            if realHeadPosition.isDiagonallyAdjacentTo(realTailPosition):
-                # echo "diagonal Left"
-                if (realHeadPosition[1] - realTailPosition[1]) >= 1:
-                    realTailPosition[1] += 1
-                else:
-                    # echo "puff"
-                    realTailPosition[1] -= 1
-                realTailPosition[0] -= 1
-            elif realHeadPosition.isTwoHorizontalStepsAheadOf(realTailPosition):
-                # echo "Horizontal Left"
-                realTailPosition = (realHeadPosition[0]+1, realTailPosition[1])
-            else:
-                # echo "none of them"
-                discard
-            tailMovements.add(realTailPosition)
-        # echo "moved Left and distance moved is: ", movement.distanceToMove(), " and head position is ", realHeadPosition, " while tail position is: ", realTailPosition
-    of 'U':
-        for eachStep in 1..movement.distanceToMove():
-            inc realHeadPosition[1]
-            if realHeadPosition.isDiagonallyAdjacentTo(realTailPosition):
-                # echo "diagonal Up"
-                realTailPosition = (realTailPosition[0]+1, realTailPosition[1]+1)
+        if hposx - tposx == 1 and hposY - tposY == 2: 
+            #head just moved 2 above and to the right
+            echo "  -- head just moved 2 above and to the right"
+            inc tposX
+            inc tposY
 
-            elif realHeadPosition.isTwoHorizontalStepsAheadOf(realTailPosition):
-                # echo "Horizontal Up"
-                realTailPosition = (realTailPosition[0], realHeadPosition[1]-1)
-            else:
-                # echo "none of them"
-                discard
-            tailMovements.add(realTailPosition)
-        # echo "moved Up and distance moved is: ", movement.distanceToMove(), " and head is ", realHeadPosition, " while tail position is: ", realTailPosition
-    of 'D':
-        for eachStep in 1..movement.distanceToMove():
-            realHeadPosition[1] -= 1
-            if realHeadPosition.isDiagonallyAdjacentTo(realTailPosition):
-                # echo "diagonal Down"
-                realTailPosition = (realTailPosition[0]+1, realTailPosition[1]-1)
+        elif tposx - hposx == 1 and hposY - tposY == 2: 
+            #head just moved 2 above and to the left
+            echo "  -- head just moved 2 above and to the left"
+            dec tposX
+            inc tposY
 
-            elif realHeadPosition.isTwoHorizontalStepsAheadOf(realTailPosition):
-                # echo "Horizontal Down"
-                realTailPosition = (realHeadPosition[0], realHeadPosition[1]+1)
-            else:
-                # echo "none of them"
-                discard
-            tailMovements.add(realTailPosition)
-        # echo "moved down and distance moved is: ", movement.distanceToMove(), " and head is ", realHeadPosition, " while tail position is: ", realTailPosition
+        elif hposX - tposX == 2 and hposY - tposY == 1:
+            #head is above and just moved 2 steps right
+            echo "  -- head is above and just moved 2 steps right"
+            inc tposX
+            inc tposY
+
+        elif tposX - hposX == 2 and hposY - tposY == 1:
+            #head is above and just moved 2 steps left
+            echo "  -- head is above and just moved 2 steps left"
+            dec tposX
+            inc tposY
+
+        elif hPosX - tPosX == 1 and tPosY - hPosY == 2:
+            #head just moved 2 below and to the right
+            echo "  -- head just moved 2 below and to the right"
+            inc tposX
+            dec tposY
+
+        elif tPosX - hPosX == 1 and tPosY - hPosY == 2:
+            #head just moved 2 below and to the left
+            echo "  -- head just moved 2 below and to the left"
+            dec tposX
+            dec tposY
+
+        elif hposX - tposX == 2 and tposy - hposy == 1:
+            #head is below and just moved 2 steps right
+            echo "  -- head is below and just moved 2 steps right"
+            inc tposX
+            dec tposY
+
+        elif tposX - hposX == 2 and tposy - hposy == 1:
+            #head is below and just moved 2 steps left
+            echo "  -- head is below and just moved 2 steps left"
+            dec tposY
+            dec tposX
+
+        else:
+            echo "       -->no diagonal case is matched"
+
+        
+    
     else:
-        echo "The direction ", movement.direction(), " is not recognised"
-    
-    
+        echo "---------=============----------"
+        echo "met a condition not covered!"
+        echo "Head X: ", hposX
+        echo "Head Y: ", hposY
+        echo "Tail X: ", tposX
+        echo "Tail Y: ", tposY
+        echo "---------=============----------"
 
-echo tailMovements.deduplicate.len
+    tailposlogger.add((tposX, tposY))
+
+
+
+proc goRight(stepSize: int) =
+    for step in 1..stepSize:
+        echo "head went right"
+        inc hposX
+        updateTail()
+
+proc goLeft(stepSize: int) =
+    for step in 1..stepSize:
+        echo "head went left"
+        dec hposX
+        updateTail()
+
+proc goUp(stepSize: int) =
+    for step in 1..stepSize:
+        echo "head went up"
+        inc hposY
+        updateTail()
+
+proc goDown(stepSize: int) =
+    for step in 1..stepSize:
+        echo "head went down"
+        dec hposY
+        updateTail()
+
+
+for instruction in data:
+    let 
+        direction = instruction[0]
+        stepSize = instruction[1]
+    case direction:
+        of "R": #go right
+            goRight(parseint stepSize)
+        of "L":#go left
+            goLeft(parseint stepSize)
+        of "U":#go up
+            goUp(parseint stepSize)
+        of "D":#go down
+            goDown(parseint stepSize)
+
+
+echo "=================================="
+echo "final positions are: "
+echo "  Head X: ", hposx
+echo "  Head Y: ", hposy
+echo "  Tail X: ", tposx
+echo "  Tail Y: ", tposy
+echo "=================================="
+
+echo "answer is ", tailposlogger.deduplicate.len
